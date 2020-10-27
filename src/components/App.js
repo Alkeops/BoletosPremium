d6f8ac625ef4782aeb220f032388d3d692290ac8
Alkeops <m_adan2012@outlook.com>
2020-10-27 13:13:00 -0600

    Listo para empezar

---
+++ src/components/App.js
@@ -0,0 +1,31 @@
+function App() {
+  return (
+    <div className="App">
+      <h1>Hola !!!Mundo</h1>
+      <h1>
+        <div>
+          <div className="div">
+            <div className="div">
+              .asi .asi
+              <div className="asi"></div>
+              <div className="div"></div>
+            </div>
+            <div className="algo">
+              <div className="algo">
+                <div className="algo">
+                  <div className="alfo">
+                    <div className="algo"></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="asi">
+            <div className="despues"></div>
+          </div>
+        </div>
+      </h1>
+    </div>
+  );
+}
+export default App;
